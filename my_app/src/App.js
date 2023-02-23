cd83a417955cdd4a5853a9d20eee9c85670b3542
Mohamed_Anwar <alhamed202024@gmail.com>
2023-02-24 01:38:12 +0300

    beginning 7

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -6,11 +6,15 @@ import './App.css';
 //import ReactDOM from 'react-dom';
 
 function App() {
+  const style = {
+    backgroundColor: 'green',
+    marginLeft: '1000px'
+  }
   return (
     // React.createElement('h1', null,'Welcome to GeeksforGeeks')
     <div className="App">
         <Car name="toyota"/>
-        <h1>hi mohamed anwar</h1>
+        <h1 style={style}>hi mohamed anwar</h1>
         <Cat name="mimi" age="3"/>
         <Cat name="mimi2" age="4"/>
         <Cat name="mimi3" age="5"/>

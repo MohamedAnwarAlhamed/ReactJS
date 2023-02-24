2d73aee30827e3ee98923d60013f956dae7ebaef
Mohamed_Anwar <alhamed202024@gmail.com>
2023-02-24 16:45:11 +0300

    beginning 8

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,11 +1,19 @@
-
-import React from 'react';
+import {React, useState} from 'react';
 import Car from './car';
 import Cat from './components/Animals';
 import './App.css';
 //import ReactDOM from 'react-dom';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const handleIncrement = () => {
+    setCount(count + 1);
+  }
+  const [animals, setAnimals] = useState([
+    {name: 'mimi', age: 25},
+    {name: 'mimi2', age: 26},
+    {name: 'mimi3', age: 27},
+  ])
   const style = {
     backgroundColor: 'green',
     marginLeft: '1000px'
@@ -15,9 +23,15 @@ function App() {
     <div className="App">
         <Car name="toyota"/>
         <h1 style={style}>hi mohamed anwar</h1>
-        <Cat name="mimi" age="3"/>
-        <Cat name="mimi2" age="4"/>
-        <Cat name="mimi3" age="5"/>
+        {
+        animals.map((animals) => (           
+            <Cat key={animals.name} name={animals.name} age={animals.age}/>
+        ))
+      }
+     
+        <button onClick={handleIncrement}>increment</button>
+        <button onClick={() => setCount(count + 1)}>increment 2</button>
+        {count}
     </div>
   );
 }
